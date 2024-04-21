2950a1a3b57d9fbcc7eaecb09ad10dbab7dc0fc0
Colleen Polka <cmpolka@wm.edu>
2024-04-20 20:45:52 -0400

    Main game loop, user input, logic added

---
+++ clawdius.py
@@ -0,0 +1,32 @@
+from logic import Minigames
+
+class Clawdius:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        # TODO figure out params to take (x, y, anything else?) and init
+        self.x = x
+        self.y = y
+        self.WIN_WIDTH = width
+        self.WIN_HEIGHT = height
+
+    def is_in_building(self, x: int, y: int):
+        """Checks if Clawdius is in any of the construction sites on the map. 
+    
+        Args: 
+            x: Clawdius' x-position on the map (in pixels)
+            y: Clawdius' y-position on the map (in pixels)
+
+        Returns:
+            The construction spot's index in the game state array
+            if in a valid construction site, else returns -1.
+        """
+
+        # Check Claw's x and y on the map against each of the
+        # construction sites. If overlap, press return that site's
+        # index, else -1
+        return 0
+
+    def move(self):
+        pass
+
+    def draw(self, window):
+        pass
---
+++ logic.py
@@ -0,0 +1,56 @@
+import pygame
+from games import snake
+from enum import Enum
+
+
+
+class Minigames(Enum):
+    """ Corresponds to game state indeces. """
+    YATES = 0
+    MONROE = 1
+    ISC = 2
+    LEMON = 3
+
+class Logic:
+
+    def __init__(self):
+        pass
+
+    def play(self, minigame: int):
+        """Begins the minigame for the building Clawdius is on.
+
+        Args: 
+            minigame: index in the game state array for that minigame
+
+        Returns:
+            1 if game was won,
+            0 if game was quit  
+        """
+
+        match minigame:
+            case Minigames.YATES.value:
+                # Play minigame 1
+                return self.play_snake()
+            case Minigames.MONROE.value:
+                # Play minigame 2
+                pass
+            case Minigames.ISC.value:
+                # Play minigame 3
+                pass
+            case Minigames.LEMON.value:
+                # Play minigame 4
+                pass
+
+        return 0
+
+
+    def play_snake(self):
+        """Plays the snake minigame. 
+        
+        Returns: 
+            True if the user reaches a score of 10, 
+            false if the player quits the game.
+        """
+        print("playing snake")
+        won = snake.main()
+        return won
