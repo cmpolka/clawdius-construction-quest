1f65a9b7d477d5ade7196609f6a27885ff867ce8
smendozatoledo <smendozatoledo@wm.edu>
2024-04-21 02:37:22 -0400

    Added empty file for connect_four.py

---
+++ games/connect_four.py
@@ -0,0 +1,17 @@
+#import statements
+
+# setup constants
+
+# define classes
+
+# create data structures
+
+# define generators, accessors, and utility methods
+
+# define drawing methods
+
+# create object entities
+
+# create main game loop
+def main ():
+    pass
