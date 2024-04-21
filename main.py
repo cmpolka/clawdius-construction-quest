a5d0289545f1a28a96d0bb76f5c566e3c08c1004
Colleen Polka <cmpolka@wm.edu>
2024-04-20 22:29:35 -0400

    Fix window resizing after game bug

--- main.py
+++ main.py
@@ -7,7 +7,26 @@ from logic import Logic
 WIN_WIDTH = 800
 WIN_HEIGHT = 560
 
-BASE_IMG = pygame.image.load(os.path.join("map", "base.png"))
+#BASE_IMG = pygame.image.load(os.path.join("map", "base.png"))
+IMGS = {
+    0:  pygame.image.load("map/base.png"),
+    8: pygame.image.load("map/base.png")    # TODO delete
+    # 1:  pygame.image.load("resources/map_1.png"),
+    # 2:  pygame.image.load("resources/map_2.png"),
+    # 3:  pygame.image.load("resources/map_3.png"),
+    # 4:  pygame.image.load("resources/map_4.png"),
+    # 5:  pygame.image.load("resources/map_5.png"),
+    # 6:  pygame.image.load("resources/map_6.png"),
+    # 7:  pygame.image.load("resources/map_7.png"),
+    # 8:  pygame.image.load("resources/map_8.png"),
+    # 9:  pygame.image.load("resources/map_9.png"),
+    # 10: pygame.image.load("resources/map_10.png"),
+    # 11: pygame.image.load("resources/map_11.png"),
+    # 12: pygame.image.load("resources/map_12.png"),
+    # 13: pygame.image.load("resources/map_13.png"),
+    # 14: pygame.image.load("resources/map_14.png"),
+    # 15: pygame.image.load("resources/map_15.png"),
+}
 
 def check_building(x: int, y: int):
     """Checks if Clawdius is in any of the construction sites on the map. 
@@ -27,11 +46,14 @@ def check_building(x: int, y: int):
 
 
 def png_to_render(state):
-    """ Returns the name of which map png to render based on the game state. 
+    """ Returns which png to render.
+
+    Calculates the game state value from the game state array 
+    and returns the corresponding png from IMGS.
 
     Args: 
         state: game state array [4]
-    
+
     Returns:
         png surface to render
     """
@@ -45,45 +67,12 @@ def png_to_render(state):
 
     png = yates + monroe + isc + lemon
 
-    # make this an array isntead with indeces as img vals
-    match png:
-        case 0:     # None
-            #return ("map_none.png")        # TODO add png names
-            pass
-        case 1:     # Lemon 
-            pass
-        case 2:     # ISC
-            pass
-        case 3:     # ISC, Lemon
-            pass
-        case 4:     # Monroe
-            pass
-        case 5:     # Monroe, Lemon
-            pass
-        case 6:     # Monroe, ISC
-            pass
-        case 7:     # Monroe, ISC, Lemon
-            pass
-        case 8:     # Yates
-            return BASE_IMG
-            pass
-        case 9:     # Yates, Lemon
-            pass
-        case 10:    # Yates, ISC
-            pass
-        case 11:    # Yates, ISC, Lemon
-            pass
-        case 12:    # Yates, Monroe
-            pass
-        case 13:    # Yates, Monroe, Lemon
-            pass
-        case 14:    # Yates, Monroe, ISC
-            pass
-        case 15:    # All
-            pass
+    return IMGS.get(png)
         
 
-def update_view(window, map, clawdius):
+def update_view(window, resized, map, clawdius):
+    if resized:
+        pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
     window.blit(map, (0, 0))
     # TODO implement
     clawdius.draw(window)
@@ -96,7 +85,8 @@ if __name__ == "__main__":
     logic = Logic()
     clawdius = Clawdius(0, 0, 0, 0)
     game_state = [0, 0, 0, 0]   # No buildings constructed yet
-    png = BASE_IMG        # TODO REPLACE WITH REAL MAP PNG NAMES
+    png = IMGS[0]        # TODO REPLACE WITH REAL MAP PNG NAMES
+    resized = False     # Window may get resized by minigames
 
     run = True
     while run:
@@ -127,14 +117,14 @@ if __name__ == "__main__":
                 minigame = clawdius.is_in_building(0, 0)    # TODO change this to clawd's coords
                 # Play the minigame if at site and hasn't won that game before 
                 if minigame > -1:
-                    print(minigame)
                     if game_state[minigame] != 1:
-                        print(game_state)
-                        print(game_state[minigame])
                         game_state[minigame] = logic.play(minigame)
                         png = png_to_render(game_state)
+                        # TODO maybe remove if we make games a popup instead of their own window
+                        resized = True
         
-        update_view(window, png, clawdius)
+        update_view(window, resized, png, clawdius)
+        resized = False
 
             
 
