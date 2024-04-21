745157f3f5012bb932de81f4cfa677152c2719ed
pattonnelson <pattonnelson7@gmail.com>
2024-04-21 01:31:33 -0400

    rect objects updated/main.py constants updated

--- main.py
+++ main.py
@@ -3,89 +3,17 @@ import os
 import pygame
 from clawdius import Clawdius
 from minigame import Minigame
+from constants import *
 
-WIN_WIDTH = 800
-WIN_HEIGHT = 560
-NEW_SIZE = (76,68)
-
-#Loading in png files and resizing building/rubble icons
-BASE_IMG = pygame.image.load(os.path.join("map", "base.png"))
-YATES = pygame.transform.scale(pygame.image.load(os.path.join("resources","yates.png")),NEW_SIZE)
-MONROE = pygame.transform.scale(pygame.image.load(os.path.join("resources","monroe.png")),NEW_SIZE)
-ISC = pygame.transform.scale(pygame.image.load(os.path.join("resources","isc.png")),NEW_SIZE)
-LEMON = pygame.transform.scale(pygame.image.load(os.path.join("resources","lemon.png")),NEW_SIZE)
-RUBBLE = pygame.transform.scale(pygame.image.load(os.path.join("resources","rubble.png")),NEW_SIZE)
-
-IMGS = {
-    0:  pygame.image.load("map/base.png"),
-    8: pygame.image.load("map/base.png")    # TODO delete
-    # 1:  pygame.image.load("resources/map_1.png"),
-    # 2:  pygame.image.load("resources/map_2.png"),
-    # 3:  pygame.image.load("resources/map_3.png"),
-    # 4:  pygame.image.load("resources/map_4.png"),
-    # 5:  pygame.image.load("resources/map_5.png"),
-    # 6:  pygame.image.load("resources/map_6.png"),
-    # 7:  pygame.image.load("resources/map_7.png"),
-    # 8:  pygame.image.load("resources/map_8.png"),
-    # 9:  pygame.image.load("resources/map_9.png"),
-    # 10: pygame.image.load("resources/map_10.png"),
-    # 11: pygame.image.load("resources/map_11.png"),
-    # 12: pygame.image.load("resources/map_12.png"),
-    # 13: pygame.image.load("resources/map_13.png"),
-    # 14: pygame.image.load("resources/map_14.png"),
-    # 15: pygame.image.load("resources/map_15.png"),
-}
-
-def check_building(x: int, y: int):
-    """Checks if Clawdius is in any of the construction sites on the map. 
-    
-    Args: 
-        x: Clawdius' x-position on the map (in pixels)
-        y: Clawdius' y-position on the map (in pixels)
-
-    Returns:
-        The construction spot's index in the game state array
-        if in a valid construction site, else returns -1.
-    """
-
-    # Check Claw's x and y on the map against each of the
-    # construction sites. If overlap, press return that site's
-    # index, else -1
-
-
-def png_to_render(state):
-    """ Returns which png to render.
-
-    Calculates the game state value from the game state array 
-    and returns the corresponding png from IMGS.
-
-    Args: 
-        state: game state array [4]
-
-    Returns:
-        png surface to render
-    """
-
-    # Get int value of game state by treating the values in the
-    # list as a binary string
-    yates = state[0] * 8
-    monroe = state[1] * 4
-    isc = state[2] * 2
-    lemon = state[3]
-
-    png = yates + monroe + isc + lemon
-
-    return IMGS.get(png)
-        
 
 def update_view(window, rerender_map, clawdius, game_state):
     if rerender_map:
         pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
         window.blit(BASE_IMG, (0, 0))
-        window.blit(YATES,(100,105)) if game_state[0]== 1 else window.blit(RUBBLE,(100,105))
-        window.blit(MONROE,(655,157)) if game_state[1] == 1 else window.blit(RUBBLE,(655,157))
-        window.blit(ISC,(345,397)) if game_state[2] == 1 else window.blit(RUBBLE,(345,397))
-        window.blit(LEMON,(513,465)) if game_state[3] == 1 else window.blit(RUBBLE,(513,465))
+        window.blit(YATES,YATES_COORD) if game_state[0]== 1 else window.blit(RUBBLE,YATES_COORD)
+        window.blit(MONROE,MONROE_COORD) if game_state[1] == 1 else window.blit(RUBBLE,MONROE_COORD)
+        window.blit(ISC,ISC_COORD) if game_state[2] == 1 else window.blit(RUBBLE,ISC_COORD)
+        window.blit(LEMON,LEMON_COORD) if game_state[3] == 1 else window.blit(RUBBLE,LEMON_COORD)
     clawdius.draw(window)
     pygame.display.update()
 
