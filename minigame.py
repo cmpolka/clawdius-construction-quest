6eedef3c10f252639966fb9cf0ef5c7002623350
pattonnelson <pattonnelson7@gmail.com>
2024-04-21 08:28:20 -0400

    Updated gamestart window

--- minigame.py
+++ minigame.py
@@ -57,31 +57,43 @@ class Minigame:
             
         MENU_MOUSE_POS = pygame.mouse.get_pos()
             
-        text = 'Snake Game'
-        button_text = 'Start Game'
+        menu_text = 'Snake Game'
         font = pygame.font.init()
         font = pygame.font.SysFont('Helvetica',size = 20)
-        MENU_TEXT = font.render(text, True, (0,0,0))
+        MENU_TEXT = font.render(menu_text, True, (0,0,0))
         MENU_RECT = MENU_TEXT.get_rect(center=(400,100))
         
-        button = font.render(button_text,True,(0,0,0))
-        button_rect = button.get_rect(center = (400,400))
-            
         screen.blit(MENU_TEXT,MENU_RECT)
-            
-        screen.blit(button,button_rect)
-            
-        pygame.display.update()  
-        pygame.time.delay(3000)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        
+        button_surface = pygame.Surface((150, 50))
+        text = font.render("Start Game",True,(0,0,0))
+        text_rect = text.get_rect(center=(button_surface.get_width()/2,button_surface.get_height()/2))
+        
+        button_rect = pygame.Rect(125,125,150,50)
+        
+        clock = pygame.time.Clock()
+        
+        while True:
+            clock.tick(60)
+            screen.fill((155,255,155))
+            for event in pygame.event.get():  
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if button_rect.collidepoint(event.pos):
+                        self.play_snake()
+            if button_rect.collidepoint(pygame.mouse.get_pos()):
+                pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
             else:
-                self.play_snake()
-        pygame.display.update()
+                pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
+                pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
+                pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
+                pygame.draw.rect(button_surface, (0, 100, 0), (1, 48, 148, 10), 2)
+            button_surface.blit(text,text_rect)
             
-            
+            screen.blit(button_surface,(button_rect.x,button_rect.y))
+            pygame.display.update()
 
     def play_snake(self):
         """Plays the snake minigame. 
