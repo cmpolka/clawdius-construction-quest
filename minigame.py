924d64f291473f3484b7d74141f1ba09e4b9799c
pattonnelson <pattonnelson7@gmail.com>
2024-04-21 07:57:11 -0400

    Adding display screen

--- minigame.py
+++ minigame.py
@@ -1,7 +1,10 @@
 import pygame
+import sys
+from button import Button
 from games import snake
 from enum import Enum
 from constants import *
+
 
 class Minigame:
 
@@ -22,7 +25,7 @@ class Minigame:
         match minigame:
             case Minigames.YATES.value:
                 # Play minigame 1
-                return self.play_snake()
+                return self.minigame_title_screen()
             case Minigames.MONROE.value:
                 # Play minigame 2
                 pass
@@ -32,25 +35,53 @@ class Minigame:
             case Minigames.LEMON.value:
                 # Play minigame 4
                 pass
-
-        return 0
     
 
     # def minigame_title_screen(self, minigame):
-    #     #minigame = pygame.display.init()
+    #     #screen = pygame.display.init()
     #     rect = pygame.Rect(150, 30, 500, 500)
-    #     minigame = pygame.display.set_mode(size=(800, 560))
+    #     screen = pygame.display.set_mode(size=(800, 560))
     #     print(minigame)
     #     minigame.fill((255, 255, 255), rect)
     #     pygame.display.update()
     #     pygame.time.delay(3000)
     #     return self.play_snake(minigame, rect)
 
-
-    # def create_minigame_surface(self, w, h):
-    #     minigame_surface = pygame.Surface((w, h))
-    #     minigame_surface.fill((0, 0, 0))
-    #     return minigame_surface
+    def minigame_title_screen(self):
+        screen = pygame.display.init()
+        rect = pygame.Rect(150,30,500,500)
+        screen = pygame.display.set_mode(size=(800,560))
+        pygame.display.update()
+        screen.fill((255,255,255),rect)
+        pygame.display.update()
+            
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+            
+        text = 'Snake Game'
+        button_text = 'Start Game'
+        font = pygame.font.init()
+        font = pygame.font.SysFont('Helvetica',size = 20)
+        MENU_TEXT = font.render(text, True, (0,0,0))
+        MENU_RECT = MENU_TEXT.get_rect(center=(400,100))
+        
+        button = font.render(button_text,True,(0,0,0))
+        button_rect = button.get_rect(center = (400,400))
+            
+        screen.blit(MENU_TEXT,MENU_RECT)
+            
+        screen.blit(button,button_rect)
+            
+        pygame.display.update()  
+        pygame.time.delay(3000)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            else:
+                self.play_snake()
+        pygame.display.update()
+            
+            
 
     def play_snake(self):
         """Plays the snake minigame. 
